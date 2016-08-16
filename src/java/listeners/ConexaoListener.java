bca11a88ae5dd748b996871f279afcefbac233df
Alisson Chiquitto <chiquitto@gmail.com>
2016-08-16 15:44:55 -0300

    Import inicial

---
+++ src/java/listeners/ConexaoListener.java
@@ -0,0 +1,18 @@
+package listeners;
+
+import br.com.chiquitto.aula.jdbcblog.Conexao;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class ConexaoListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        Conexao.setUrl("jdbc:sqlite:__DIRETORIO__/data/escola.sqlite.db");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+    
+}
